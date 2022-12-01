50f954d5cafa1046b219131f363f61b4661b15bd
jeraldgan <jeraldgantc@gmail.com>
2022-12-02 00:38:55 +0800

    add gitignore for section 3

--- 3-programming-concepts/target/debug/deps/programming_concepts-663d7e4fdb3a0409.d
+++
@@ -1,5 +0,0 @@
-/Users/jerald/Git/learn/learn-rust/3-programming-concepts/target/debug/deps/programming_concepts-663d7e4fdb3a0409.rmeta: src/main.rs
-
-/Users/jerald/Git/learn/learn-rust/3-programming-concepts/target/debug/deps/programming_concepts-663d7e4fdb3a0409.d: src/main.rs
-
-src/main.rs:
--- 3-programming-concepts/target/debug/programming-concepts.d
+++
@@ -1,1 +0,0 @@
-/Users/jerald/Git/learn/learn-rust/3-programming-concepts/target/debug/programming-concepts: /Users/jerald/Git/learn/learn-rust/3-programming-concepts/src/main.rs
